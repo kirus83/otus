477d53901944773ec6b848a70b098de43b1b186a
kirus83 <kirya555@mail.ru>
2021-10-24 11:30:55 +0300

    First iteration for home work №3

--- Scoring API/test.py
+++ Scoring API/test.py
@@ -27,7 +27,9 @@ class TestSuite(unittest.TestCase):
 
     def set_valid_auth(self, request):
         if request.get("login") == api.ADMIN_LOGIN:
-            request["token"] = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H").encode('utf-8') + bytearray(api.ADMIN_SALT,'utf8')).hexdigest()
+            request["token"] = hashlib.sha512(
+                datetime.datetime.now().strftime("%Y%m%d%H").encode("utf-8") + bytearray(api.ADMIN_SALT,
+                                                                                         "utf8")).hexdigest()
         else:
             msg = request.get("account", "") + request.get("login", "") + api.SALT
             request["token"] = hashlib.sha512(msg.encode('utf-8')).hexdigest()
